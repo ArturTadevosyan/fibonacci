df024f84b531ab7e07eebe0098d802aaadda3b94
ArturTadevosyan <arturtadevosyan926@gmail.com>
2023-08-02 13:18:55 +0400

    update code

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,22 +5,33 @@ public class Fibonacci {
     //1 1 2 3 5 8 13 21 34 55 89
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter Fibonacci Index: ");
-        int index = scanner.nextInt();
+
+        boolean isActive = true;
 
         int firstNumber = 1;
         int secondNumber = 1;
         int result = 1;
 
-        if (index == 1 || index == 2) {
-            System.out.println(result);
-        } else {
-            for (int i = 2; i < index; i++) {
-                result = firstNumber + secondNumber;
-                firstNumber = secondNumber;
-                secondNumber = result;
+        while (isActive) {
+            System.out.print("Enter Fibonacci Index: ");
+            int index = scanner.nextInt();
+            if (index != 0) {
+                if (index == 1 || index == 2) {
+                    System.out.println(result);
+                } else {
+                    for (int i = 3; i <= index; i++) {
+                        result = firstNumber + secondNumber;
+                        firstNumber = secondNumber;
+                        secondNumber = result;
+                    }
+                    System.out.println("Fibonacci: " + index + " index number = " + result);
+                    result = 1;
+                    firstNumber = 1;
+                    secondNumber = 1;
+                }
+            } else {
+                isActive = false;
             }
-            System.out.println("Fibonacci: " + index + " index number = " + result);
         }
     }
 }
